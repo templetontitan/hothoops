71685f29926452c5b1d60fa4d810edb507d392e3
1250316Sofia <sofietor@users.noreply.github.com>
2017-05-18 15:00:38 -0700

    Update game.js

--- game.js
+++ game.js
@@ -44,7 +44,7 @@ Hoops.MainMenu.prototype = {
 
     create: function () {
 
-        this.stage.backgroundColor = 0x100420;
+        this.stage.backgroundColor = 0x100406;
 
         var logo = this.add.image(this.world.centerX, 80, 'logo');
         logo.anchor.x = 0.5;
