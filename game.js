3db0b8dd626c15e86814503afc710d461c1d5411
1250316Sofia <sofietor@users.noreply.github.com>
2017-05-23 13:51:31 -0700

    Update game.js

--- game.js
+++ game.js
@@ -105,7 +105,7 @@ Hoops.Game.prototype = {
 
     create: function () {
 
-        this.stage.backgroundColor = 0x100438;
+        this.stage.backgroundColor = 0x006666;
 
         this.board = this.add.sprite(700, 200, 'board');
 
